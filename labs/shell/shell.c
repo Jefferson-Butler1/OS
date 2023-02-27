e47bf8ef8f4b3c9c15188f72260d73aa3c8c98f5
Jefferson-Butler1 <jefferson.butler@icloud.com>
2023-02-27 22:42:25 +0000

    added output redirection

--- labs/shell/shell.c
+++ labs/shell/shell.c
@@ -5,45 +5,38 @@
 #include <readline/history.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "parse.h"
 
 void err(char* message, int code){
 	fprintf(stderr, "Error: %s\n", message);
-	exit(code);
+	if(code != 0) exit(code);
 }
 
 int main(){
 	char * line;
 	while ((line = readline("> ")) != NULL){
 		struct root *r = parse(line);
-	     	if(!r->valid){
-		   	fprintf(stderr, "Error: Bad Command");
-	   	   	continue;
-		}
-		/*
-		if(strcmp(*r -> first_command -> argv, "exit") == 0){
-    			parse_end(r);
- 			free(line);
-			break;
-		}*/
+	     	if(!r->valid){ err("Error: Bad Command", 0);continue;}
 		struct command *cmd = r -> first_command;
-		int thisI = -1, 
-		    thisO = -1,
-		    nextI = -1,
-		    fds[2], 
-		    rc;		
+		int thisI = -1, thisO = -1, nextI = -1, fds[2], rc, file;		
 		while(cmd != NULL){
 			if(cmd -> next != NULL){
 				if(pipe(fds) < 0) err("bad pipe", 1);
 				thisO = fds[1];
 				nextI = fds[0];
 			}
+			if(cmd -> outfile){
+				thisO = open(cmd -> outfile, O_WRONLY | O_CREAT, 0666);
+			}
 			rc = fork();
 			if(rc == 0){
 				if(thisI != -1){close(0); dup(thisI); close(thisI);}
 				if(thisO != -1){close(1); dup(thisO); close(thisO);}
 				execvp(*cmd -> argv, cmd -> argv);
+				err("command not found", 1);
 			}
 			if(cmd -> next != NULL){
 				if(thisI != -1)	close(thisI);
@@ -53,21 +46,8 @@ int main(){
 			thisI = thisO = -1;
 			if(nextI != -1){thisI = nextI; nextI = -1;}
 			wait(NULL);
-
 		}
     		parse_end(r);
     		free(line);
     	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
